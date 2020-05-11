51a98c2db98ddd88cd51ba1c8b8256949fd2a0c1
woong.jang <54804710+noleplay81@users.noreply.github.com>
2020-05-12 07:39:20 +0900

    Add outline for polyline. (#78)
    * Add outline for polyline.
    
    * change log and version update.
    
    Co-authored-by: WoongJang <Woong.Jang@partnet.bmw.co.kr>
    Co-authored-by: Dave Krawczyk <dave.krawczyk@gmail.com>

--- packages/atlas/lib/src/polyline.dart
+++ packages/atlas/lib/src/polyline.dart
@@ -19,12 +19,24 @@ class Polyline {
   /// The default value is 10.
   final int width;
 
-  const Polyline({
-    @required this.id,
-    @required this.points,
-    this.color = Colors.black,
-    this.width = 10,
-  })  : assert(id != null),
+  /// The 'Outline Color' of the line.
+  /// The default color is black.
+  final Color outlineColor;
+
+  /// Width of the outline of the polyline.
+  ///
+  /// The width is constant and independent of the camera's zoom level.
+  /// The default value is 0.
+  final int outlineWidth;
+
+  const Polyline(
+      {@required this.id,
+      @required this.points,
+      this.color = Colors.black,
+      this.width = 10,
+      this.outlineColor = Colors.black,
+      this.outlineWidth = 0})
+      : assert(id != null),
         assert(points != null);
 
   @override
@@ -35,10 +47,17 @@ class Polyline {
     return id == typedOther.id &&
         points == typedOther.points &&
         color == typedOther.color &&
-        width == typedOther.width;
+        width == typedOther.width &&
+        outlineColor == typedOther.outlineColor &&
+        outlineWidth == typedOther.outlineWidth;
   }
 
   @override
   int get hashCode =>
-      id.hashCode ^ points.hashCode ^ color.hashCode ^ width.hashCode;
+      id.hashCode ^
+      points.hashCode ^
+      color.hashCode ^
+      width.hashCode ^
+      outlineColor.hashCode ^
+      outlineWidth.hashCode;
 }
