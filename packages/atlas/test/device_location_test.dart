1ebb958d917d7a01b854dddd2a10ac6407637730
oguibueno <oguibueno@gmail.com>
2021-02-09 11:17:33 -0300

    hashcode equals override

--- packages/atlas/test/device_location_test.dart
+++ packages/atlas/test/device_location_test.dart
@@ -19,10 +19,12 @@ main() {
           longitude: 1.0,
         ),
         accuracy: 1.0,
+        altitude: 1.0,
       );
       expect(deviceLocation.target.latitude, 1.0);
       expect(deviceLocation.target.longitude, 1.0);
       expect(deviceLocation.accuracy, 1.0);
+      expect(deviceLocation.altitude, 1.0);
     });
 
     test('accuracy should be zero when not provided', () {
@@ -44,6 +46,28 @@ main() {
         accuracy: null,
       );
       expect(deviceLocation.accuracy, null);
+    });
+
+    test('different instances with same properties should be equal', () {
+      final deviceLocationA = DeviceLocation(
+        target: LatLng(
+          latitude: 1.0,
+          longitude: 1.0,
+        ),
+        accuracy: 1.0,
+        altitude: 1.0,
+      );
+
+      final deviceLocationB = DeviceLocation(
+        target: LatLng(
+          latitude: 1.0,
+          longitude: 1.0,
+        ),
+        accuracy: 1.0,
+        altitude: 1.0,
+      );
+
+      expect(deviceLocationA, deviceLocationB);
     });
   });
 }
