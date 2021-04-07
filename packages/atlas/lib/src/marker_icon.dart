d7509a42cb171af60902705c2b56e96c84f5a93f
oguibueno <oguibueno@gmail.com>
2021-04-07 13:51:13 -0300

    added assetbytes support to marker icon object

--- packages/atlas/lib/src/marker_icon.dart
+++ packages/atlas/lib/src/marker_icon.dart
@@ -1,23 +1,27 @@
-import 'package:flutter/foundation.dart';
+import 'dart:typed_data';
 
 /// Optional Marker icon.
 class MarkerIcon {
-  /// The [assetName] argument must not be null. It should name the main asset
+  /// The [assetName] argument must not be null if [assetBytes] is null. It should name the main asset
   /// from the set of images to choose from. File should be of type png
   final String assetName;
 
-  /// The [width] argument is optional. It is the desired width in pixels.
+  /// The [assetBytes] argument must not be null if [assetName] is null.
+  /// File should be of type png
+  final Uint8List assetBytes;
 
+  /// The [width] argument is optional. It is the desired width in pixels.
   final int width;
 
   /// The [height] argument is optional. It is the desired height in pixels.
   final int height;
 
   const MarkerIcon({
-    @required this.assetName,
+    this.assetName,
+    this.assetBytes,
     this.width = 0,
     this.height = 0,
-  }) : assert(assetName != null);
+  }) : assert(assetName != null || assetBytes != null);
 
   @override
   bool operator ==(Object other) {
@@ -26,7 +30,8 @@ class MarkerIcon {
     final MarkerIcon typedOther = other;
     return assetName == typedOther.assetName &&
         width == typedOther.width &&
-        height == typedOther.height;
+        height == typedOther.height &&
+        assetBytes == typedOther.assetBytes;
   }
 
   @override
@@ -34,5 +39,6 @@ class MarkerIcon {
       runtimeType.hashCode ^
       assetName.hashCode ^
       width.hashCode ^
-      height.hashCode;
+      height.hashCode ^
+      assetBytes.hashCode;
 }
