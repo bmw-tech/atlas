f2974df51932f5777e6cfce2d3d823f72ee34e29
Guilherme Galambas <ggalambas@gmail.com>
2024-04-01 10:40:30 +0100

    added opcaity and counter color

--- packages/atlas/lib/src/heatmap/heatmap.dart
+++ packages/atlas/lib/src/heatmap/heatmap.dart
@@ -1,13 +1,30 @@
 import 'package:atlas/src/heatmap/heatcolor.dart';
+import 'package:flutter/material.dart';
 
 import 'heatpoint.dart';
 
 /// Visualization to depict the intensity of data at geographical points.
-
 class Heatmap {
-  final Set<Heatpoint> heatpoints;
-  final Heatcolor? heatcolor;
+  /// The points where to build the heatmaps
+  final Set<Heatpoint> points;
+
+  /// Defines the color of each pixel based on its density value in a heatmap
+  final Heatcolor? color;
+
+  /// The global opacity at which the heatmap layer will be drawn.
+  final double opacity;
+
+  /// Whether to show or not the counter of points
   final bool showCounter;
 
-  const Heatmap({required this.heatpoints, this.heatcolor, this.showCounter = false});
+  /// The color with which the counter will be drawn.
+  final Color counterColor;
+
+  const Heatmap({
+    required this.points,
+    this.color,
+    this.opacity = 1,
+    this.showCounter = false,
+    this.counterColor = Colors.black,
+  });
 }
